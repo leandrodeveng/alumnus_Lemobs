a784be1aa89a95b553620b9a4a5cc1a54aafdeca
leandrodeveng <leandro.lt187@gmail.com>
2020-02-11 18:50:40 -0300

    format-date-bug-fix

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,14 @@
+import { BadRequestException } from "@nestjs/common";
+
 export function formatedDate(dateStr) {
   const darr = dateStr.split("-");
-  var dobj = new Date(parseInt(darr[2]),parseInt(darr[1])-1,parseInt(darr[0]));
-  return dobj
+  var reg = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+
+  if (dateStr.match(reg)) {
+    var dobj = new Date(parseInt(darr[2]),parseInt(darr[1])-1,parseInt(darr[0]));
+    return dobj
+  }
+  else {
+    throw new BadRequestException('Please enter a valid date with dd-mm-yyyy format')
+  }
 }
